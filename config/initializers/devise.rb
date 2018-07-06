dd23e113fa059c7b02c0cf12d74c79a3a071a4cd
ebkaylor <ebkaylor@knox.edu>
2018-07-06 16:25:57 -0500

    Changed callback url to heroku app one

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -280,5 +280,5 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, "256350441807430", "c67fb7771437653d2b7f71b169c90ab4", callback_url: "http://localhost:5000/users/auth/facebook/callback"
+  config.omniauth :facebook, "256350441807430", "c67fb7771437653d2b7f71b169c90ab4", callback_url: "https://enigmatic-bayou-42469.herokuapp.com/users/auth/facebook/callback"
 end
